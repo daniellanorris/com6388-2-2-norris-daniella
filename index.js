0bf2153cd753d384ec7202631b207b9257c7ac88
daniellanorris <daniellanorris@ufl.edu>
2022-09-08 12:38:04 -0400

    adding my solution to the index.js file

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 function greet() {
-    var username = prompt("What is your name?");
-    var greeting = alert("Hello,  " + username); 
+    var username = prompt("What is your name?", '');
+    alert(`Hello, ${username}`); 
 
-    var age = prompt("How old are you?");
+
+    var age = prompt("How old are you?", '');
     var numberAge = parseInt(age);
 
-    var question;
-    if (confirm("Have you already had a birthday this year?") == true) {
-        question = "Cool!";
-    } else {
-        question = "Soon enough!";
-    }
-
-    var yearMsg = alert("You were born in  " + calcYear) 
+    var question  = confirm("Have you already had a birthday this year?");
     if(question === true) {
         calcYear = new Date().getFullYear() - numberAge;
     } else {
-        calcYear = (new Date().getFullYear() + 1) - numberAge;
+        calcYear = new Date().getFullYear()  - (numberAge + 1);
     }
-
+    alert("Your birthyear is  " + calcYear);
+    
 }
